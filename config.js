78c2ab020929d910440d00aaf25df648fa07e7a2
aekbote <88759575+aekbote@users.noreply.github.com>
2022-08-17 08:38:59 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 import firebase from "firebase";
 require("@firebase/firestore");
 
-//Paste your firebaseConfig here
 
-//
+const firebaseConfig = {
+    apiKey: "AIzaSyCI3KGowKY62svPX8cNifsQ-5piILXbjzI",
+    authDomain: "c71-hw.firebaseapp.com",
+    projectId: "c71-hw",
+    storageBucket: "c71-hw.appspot.com",
+    messagingSenderId: "462329375634",
+    appId: "1:462329375634:web:73ba63820624232930ef4b"
+  };
+  
+
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.firestore();
